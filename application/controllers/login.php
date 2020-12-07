12e1ff1c8d0a95c6ecb6251258a9fae987c69a94
vanditpopat <vandit.popat@somaiya.edu>
2020-12-07 23:04:54 +0530

    Buy page

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -6,7 +6,7 @@ class Login extends MY_Controller{
 	//TESTING PURPOSE ONLY
 	public function testing()
 		{
-			$this->load->view('user/prodhist_user');
+			$this->load->view('user/buy_user');
 		}
 	//TEST YOUR VIEWS USING THE ABOVE FUNCTION
 
--- application/views/user/buy_user.php
+++ application/views/user/buy_user.php
@@ -1,2 +1,129 @@
 <?php include('header_user.php');?>
+<br>
+<body>
+<ul class="nav nav-pills" style="margin-left:25%;">
+  
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Brand</a>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Action</a>
+      <a class="dropdown-item" href="#">Another action</a>
+      <a class="dropdown-item" href="#">Something else here</a>
+      <div class="dropdown-divider"></div>
+      <a class="dropdown-item" href="#">Separated link</a>
+    </div>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Price</a>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Action</a>
+      <a class="dropdown-item" href="#">Another action</a>
+      <a class="dropdown-item" href="#">Something else here</a>
+      <div class="dropdown-divider"></div>
+      <a class="dropdown-item" href="#">Separated link</a>
+    </div>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Review</a>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Action</a>
+      <a class="dropdown-item" href="#">Another action</a>
+      <a class="dropdown-item" href="#">Something else here</a>
+      <div class="dropdown-divider"></div>
+      <a class="dropdown-item" href="#">Separated link</a>
+    </div>
+  </li>
+</ul>
+<br>
+<br>
+
+    <div class="container">
+    <ol class="breadcrumb" style="width: 250px;">
+        <li class="breadcrumb-item"><a href="#">User</a></li>
+        <li class="breadcrumb-item active">Buying</li>
+    </ol>
+    <br>
+    <h2 class="text-primary">Refurbished Product</h2>
+    <br><br>
+        <div class="row">
+            
+        <div class="col-6" style="margin-bottom: 50px;">
+                <div class="row">
+                    <div class="mycol4">
+                        <p><strong>Dell Windows 10</strong>
+                        <p>Service By:<strong> Laptop</strong></p>
+                        <p>Date:<strong> 3/11/2020</strong></p>
+                        <p>Price:<strong>3</strong></p>
+                    </div>
+                    <div class="col-4" style="margin-right: 10px;">
+                        <img src="http://[::1]/CI_EWM/uploads/user/download.png" alt="laptop image" height="135px" width="135px">
+                    </div>
+                </div>
+                <div>
+                    <div >    <button class="btn btn-info">Details</button> 
+                              <button type="button" class="btn btn-success">Add to Cart</button>  
+                    </div>
+        
+        </div> 
+        </div> 
+        <div class="col-6" style="margin-bottom: 50px;">
+                <div class="row">
+                    <div class="mycol4">
+                        <p><strong>Dell Windows 10</strong>
+                        <p>Service By:<strong> Laptop</strong></p>
+                        <p>Date:<strong> 3/11/2020</strong></p>
+                        <p>Price:<strong>3</strong></p>
+                    </div>
+                    <div class="col-4" style="margin-right: 10px;">
+                        <img src="http://[::1]/CI_EWM/uploads/user/download.png" alt="laptop image" height="135px" width="135px">
+                    </div>
+                </div>
+                <div>
+                    <div >    <button class="btn btn-info">Details</button> 
+                              <button type="button" class="btn btn-success">Add to Cart</button>  
+                    </div>
+        
+        </div>
+        </div>
+        <div class="col-6" style="margin-bottom: 50px;">
+                <div class="row">
+                    <div class="mycol4">
+                        <p><strong>Dell Windows 10</strong>
+                        <p>Service By:<strong> Laptop</strong></p>
+                        <p>Date:<strong> 3/11/2020</strong></p>
+                        <p>Price:<strong>3</strong></p>
+                    </div>
+                    <div class="col-4" style="margin-right: 10px;">
+                        <img src="http://[::1]/CI_EWM/uploads/user/download.png" alt="laptop image" height="135px" width="135px">
+                    </div>
+                </div>
+                <div>
+                    <div >    <button class="btn btn-info">Details</button> 
+                              <button type="button" class="btn btn-success">Add to Cart</button>  
+                    </div>
+        
+        </div>
+        </div>
+        <div class="col-6" style="margin-bottom: 50px;">
+                <div class="row">
+                    <div class="mycol4">
+                        <p><strong>Dell Windows 10</strong>
+                        <p>Service By:<strong> Laptop</strong></p>
+                        <p>Date:<strong> 3/11/2020</strong></p>
+                        <p>Price:<strong>3</strong></p>
+                    </div>
+                    <div class="col-4" style="margin-right: 10px;">
+                        <img src="http://[::1]/CI_EWM/uploads/user/download.png" alt="laptop image" height="135px" width="135px">
+                    </div>
+                </div>
+                <div>
+                    <div >    <button class="btn btn-info">Details</button> 
+                              <button type="button" class="btn btn-success">Add to Cart</button>  
+                    </div>
+        
+        </div>  
+        </div>
+    </div>
+    </div>
+</body>
 <?php include('footer.php');?>
