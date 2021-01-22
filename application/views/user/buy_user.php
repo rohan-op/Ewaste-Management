09ae4ffa2a7c55a8da3100cd4ea08fa46d9e584a
rohan-op <rohan27@somaiya.edu>
2021-01-22 20:50:08 +0530

    dummy card view for users to display products

--- application/views/user/buy_user.php
+++ application/views/user/buy_user.php
@@ -1,47 +1,45 @@
 <?php include('header_user.php');?>
 <br>
 <body>
-<ul class="nav nav-pills" style="margin-left:25%;">
-  
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Brand</a>
-    <div class="dropdown-menu">
-      <a class="dropdown-item" href="#">Action</a>
-      <a class="dropdown-item" href="#">Another action</a>
-      <a class="dropdown-item" href="#">Something else here</a>
-      <div class="dropdown-divider"></div>
-      <a class="dropdown-item" href="#">Separated link</a>
-    </div>
-  </li>
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Price</a>
-    <div class="dropdown-menu">
-      <a class="dropdown-item" href="#">Action</a>
-      <a class="dropdown-item" href="#">Another action</a>
-      <a class="dropdown-item" href="#">Something else here</a>
-      <div class="dropdown-divider"></div>
-      <a class="dropdown-item" href="#">Separated link</a>
-    </div>
-  </li>
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Review</a>
-    <div class="dropdown-menu">
-      <a class="dropdown-item" href="#">Action</a>
-      <a class="dropdown-item" href="#">Another action</a>
-      <a class="dropdown-item" href="#">Something else here</a>
-      <div class="dropdown-divider"></div>
-      <a class="dropdown-item" href="#">Separated link</a>
-    </div>
-  </li>
-</ul>
-<br>
-<br>
-
     <div class="container">
     <ol class="breadcrumb" style="width: 250px;">
         <li class="breadcrumb-item"><a href="#">User</a></li>
         <li class="breadcrumb-item active">Buying</li>
     </ol>
+    <br>
+    <ul class="nav nav-pills">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Brand</a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Separated link</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Price</a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Separated link</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Review</a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Separated link</a>
+        </div>
+      </li>
+    </ul>
+    <br>
     <br>
     <h2 class="text-primary">Refurbished Product</h2>
     <br><br>
@@ -49,6 +47,7 @@
         <?php if( count($products) ):
         $count=$this->uri->segment(3);
         foreach ($products as $products): ?>
+
         <div class="col-6" style="margin-bottom: 50px;">
                 <div class="row">
                     <div class="mycol4">
@@ -66,6 +65,8 @@
                   <?= anchor("user/addtoCart/{$products->p_id}",'Add to Cart',['class'=>'btn btn-success']);?>
                 </div> 
             </div> 
+
+
             <?php endforeach; ?>
             <?php else: ?>
                 <tr>
@@ -73,6 +74,116 @@
                 </tr>
             <?php endif; ?>   
         </div>
+
+        <!-- TEST CARD VIEW FOR PRODUCTS -->
+        <div class="row">
+          <div class="col-6" style="margin-bottom: 40px;">    
+            <div class="card border-primary mb-3" style="max-width: 25rem;border: none; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2); transition: 0.3s;">        
+              <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img class="d-block w-100" src="<?= $products->photo1 ?>" alt="First slide">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="<?= $products->photo1 ?>" alt="Second slide">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="<?= $products->photo1 ?>" alt="Third slide">
+                  </div>
+                </div>
+              </div>
+              <div class="card-body">
+                <h4 class="card-title"><?= $products->p_name; ?></h4>
+                <p class="card-text">Type:<strong><?= $products->p_type; ?></strong></p>
+                <p class="card-text">Date:<strong> 3/11/2020</strong></p>
+                <p class="card-text">Price:<strong><?= $products->p_price; ?></strong></p>
+                <div>
+                  <table>
+                    <tr>
+                      <td>
+                        <?= anchor("user/productDetails/{$products->p_id}",'More Details',['class'=>'btn btn-info']);?>
+                      </td>
+                      <td>
+                        <?= anchor("user/addtoCart/{$products->p_id}",'Add to Cart',['class'=>'btn btn-success']);?>
+                      </td>
+                    </tr>
+                  </table>
+                </div> 
+              </div>
+            </div>
+          </div>
+
+          <div class="col-6" style="margin-bottom: 40px;">    
+            <div class="card border-primary mb-3" style="max-width: 25rem;border: none; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2); transition: 0.3s;">        
+              <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img class="d-block w-100" src="<?= $products->photo1 ?>" alt="First slide">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="<?= $products->photo1 ?>" alt="Second slide">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="<?= $products->photo1 ?>" alt="Third slide">
+                  </div>
+                </div>
+              </div>
+              <div class="card-body">
+                <h4 class="card-title"><?= $products->p_name; ?></h4>
+                <p class="card-text">Type:<strong><?= $products->p_type; ?></strong></p>
+                <p class="card-text">Date:<strong> 3/11/2020</strong></p>
+                <p class="card-text">Price:<strong><?= $products->p_price; ?></strong></p>
+                <div>
+                  <table>
+                    <tr>
+                      <td>
+                        <?= anchor("user/productDetails/{$products->p_id}",'More Details',['class'=>'btn btn-info']);?>
+                      </td>
+                      <td>
+                        <?= anchor("user/addtoCart/{$products->p_id}",'Add to Cart',['class'=>'btn btn-success']);?>
+                      </td>
+                    </tr>
+                  </table>
+                </div> 
+              </div>
+            </div>
+          </div>
+
+          <div class="col-6" style="margin-bottom: 40px;">    
+            <div class="card border-primary mb-3" style="max-width: 25rem;border: none; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2); transition: 0.3s;">        
+              <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img class="d-block w-100" src="<?= $products->photo1 ?>" alt="First slide">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="<?= $products->photo1 ?>" alt="Second slide">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="<?= $products->photo1 ?>" alt="Third slide">
+                  </div>
+                </div>
+              </div>
+              <div class="card-body">
+                <h4 class="card-title"><?= $products->p_name; ?></h4>
+                <p class="card-text">Type:<strong><?= $products->p_type; ?></strong></p>
+                <p class="card-text">Date:<strong> 3/11/2020</strong></p>
+                <p class="card-text">Price:<strong><?= $products->p_price; ?></strong></p>
+                <div>
+                  <table>
+                    <tr>
+                      <td>
+                        <?= anchor("user/productDetails/{$products->p_id}",'More Details',['class'=>'btn btn-info']);?>
+                      </td>
+                      <td>
+                        <?= anchor("user/addtoCart/{$products->p_id}",'Add to Cart',['class'=>'btn btn-success']);?>
+                      </td>
+                    </tr>
+                  </table>
+                </div> 
+              </div>
+            </div>
+          </div>
 
       <div>
         <?= $this->pagination->create_links(); ?>
