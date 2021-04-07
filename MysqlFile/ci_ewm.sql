aa26ca7a86cc63072213841f223c5f9b965d0e3e
rohan-op <rohan27@somaiya.edu>
2021-04-07 18:19:07 +0530

    updated database

--- MysqlFile/ci_ewm.sql
+++ MysqlFile/ci_ewm.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 07, 2021 at 12:10 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.2.28
+-- Generation Time: Apr 07, 2021 at 02:48 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -41,11 +41,10 @@ CREATE TABLE `ewaste` (
   `s_id` int(11) NOT NULL,
   `problem` varchar(4000) NOT NULL,
   `service_feedback` varchar(4000) NOT NULL,
+  `r_feedback` varchar(4000) NOT NULL,
   `r_id` int(11) NOT NULL,
   `s_creditpoints` int(11) NOT NULL,
   `r_creditpoints` int(11) NOT NULL,
-  `s_info` varchar(500) NOT NULL,
-  `r_info` varchar(500) NOT NULL,
   `buy_nobuy` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -53,14 +52,14 @@ CREATE TABLE `ewaste` (
 -- Dumping data for table `ewaste`
 --
 
-INSERT INTO `ewaste` (`e_id`, `u_id`, `e_type`, `e_name`, `e_age`, `e_quantity`, `e_specs`, `e_img`, `date`, `s_id`, `problem`, `service_feedback`, `r_id`, `s_creditpoints`, `r_creditpoints`, `s_info`, `r_info`, `buy_nobuy`) VALUES
-(9, 1, 'laptop/pc', 'IphoneX', 12, 1, 'Apple A11 Bionic (10 nm)\r\nCPU	Hexa-core 2.39 GHz (2x Monsoon + 4x Mistral)', 'http://[::1]/CI_EWM/uploads/ewaste/iphone.jpg', '2020-12-07 17:38:09', 1, '', '', 1, 0, 0, '', '', 1),
-(10, 1, 'tv', 'Sony 21009', 20, 1, 'Screen blurred\r\npictures black', 'http://[::1]/CI_EWM/uploads/ewaste/tv.jpeg', '2021-04-06 12:24:18', 1, 'chalo', 'chalo', 0, 0, 0, '', '', 0),
-(11, 1, 'mobile', 'Samsung M31', 24, 1, '8gb ram\r\n20mp front camera\r\n20 mp back camera\r\nandroid 10', 'http://[::1]/CI_EWM/uploads/ewaste/m31.JPG', '2021-01-09 09:48:45', 1, 'display', 'replaced display', 0, 0, 0, '', '', 1),
-(12, 1, 'tv', 'MI TV A4 Pro', 12, 2, 'Resolution: HD Ready Android TV (1366x768) | Refresh Rate: 60 hertz\r\nConnectivity: 3 HDMI ports to connect set top box, Blu Ray players, gaming console | 2 USB ports to connect hard drives and other USB devices', 'http://[::1]/CI_EWM/uploads/ewaste/mi.JPG', '2021-01-09 09:51:58', 1, 'bye', 'byr', 0, 0, 0, '', '', 1),
-(13, 1, 'mobile', 'OnePlus 7', 24, 5, '10gb ram\r\n20mp front camera\r\n30mp backcamera\r\n10W fast charging\r\nFHD+ display\r\n', 'http://[::1]/CI_EWM/uploads/ewaste/oneplus.JPG', '2021-01-14 15:58:12', 0, '', '', 0, 0, 0, '', '', 1),
-(14, 1, 'tv', 'Redmi 4A', 12, 1, '4gb ram', 'http://[::1]/CI_EWM/uploads/ewaste/m314.JPG', '2021-01-16 17:57:57', 0, '', '', 0, 0, 0, '', '', 1),
-(15, 1, 'mobile', 'Redmi 4000', 12, 2, '48MP rear camera with ultra-wide, macro, depth sensor, portrait, night mode, ai scene recognition, hdr, pro mode | 13MP front camera\r\n16.5862 centimeters (6.53-inch) FHD+ display with multi-touch capacitive touchscreen with 2340 x 1080 pixels resolution | 19.5:9 aspect ratio\r\nMemory, Storage & SIM: 4GB RAM | 64GB internal memory expandable up to 512GB |', 'http://[::1]/CI_EWM/uploads/ewaste/nokia4.JPG', '2021-03-27 18:44:54', 1, '', '', 0, 0, 0, '', '', 1);
+INSERT INTO `ewaste` (`e_id`, `u_id`, `e_type`, `e_name`, `e_age`, `e_quantity`, `e_specs`, `e_img`, `date`, `s_id`, `problem`, `service_feedback`, `r_feedback`, `r_id`, `s_creditpoints`, `r_creditpoints`, `buy_nobuy`) VALUES
+(9, 1, 'laptop/pc', 'IphoneX', 12, 1, 'Apple A11 Bionic (10 nm)\r\nCPU	Hexa-core 2.39 GHz (2x Monsoon + 4x Mistral)', 'http://[::1]/CI_EWM/uploads/ewaste/iphone.jpg', '2020-12-07 17:38:09', 1, '', '', '', 1, 0, 0, 1),
+(10, 1, 'tv', 'Sony 21009', 20, 1, 'Screen blurred\r\npictures black', 'http://[::1]/CI_EWM/uploads/ewaste/tv.jpeg', '2021-04-06 12:24:18', 1, 'chalo', 'chalo', '', 0, 0, 0, 0),
+(11, 1, 'mobile', 'Samsung M31', 24, 1, '8gb ram\r\n20mp front camera\r\n20 mp back camera\r\nandroid 10', 'http://[::1]/CI_EWM/uploads/ewaste/m31.JPG', '2021-01-09 09:48:45', 1, 'ok', 'ok', '', 0, 1, 0, 1),
+(12, 1, 'tv', 'MI TV A4 Pro', 12, 2, 'Resolution: HD Ready Android TV (1366x768) | Refresh Rate: 60 hertz\r\nConnectivity: 3 HDMI ports to connect set top box, Blu Ray players, gaming console | 2 USB ports to connect hard drives and other USB devices', 'http://[::1]/CI_EWM/uploads/ewaste/mi.JPG', '2021-01-09 09:51:58', 1, 'bye', 'byr', '', 0, 0, 0, 1),
+(13, 1, 'mobile', 'OnePlus 7', 24, 5, '10gb ram\r\n20mp front camera\r\n30mp backcamera\r\n10W fast charging\r\nFHD+ display\r\n', 'http://[::1]/CI_EWM/uploads/ewaste/oneplus.JPG', '2021-01-14 15:58:12', 0, '', '', '', 0, 0, 0, 1),
+(14, 1, 'tv', 'Redmi 4A', 12, 1, '4gb ram', 'http://[::1]/CI_EWM/uploads/ewaste/m314.JPG', '2021-01-16 17:57:57', 0, '', '', '', 0, 0, 0, 1),
+(15, 1, 'mobile', 'Redmi 4000', 12, 2, '48MP rear camera with ultra-wide, macro, depth sensor, portrait, night mode, ai scene recognition, hdr, pro mode | 13MP front camera\r\n16.5862 centimeters (6.53-inch) FHD+ display with multi-touch capacitive touchscreen with 2340 x 1080 pixels resolution | 19.5:9 aspect ratio\r\nMemory, Storage & SIM: 4GB RAM | 64GB internal memory expandable up to 512GB |', 'http://[::1]/CI_EWM/uploads/ewaste/nokia4.JPG', '2021-03-27 18:44:54', 1, '', '', '', 0, 0, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -200,25 +199,6 @@ INSERT INTO `service` (`id`, `fname`, `cname`, `role`, `contact`, `email`, `addr
 -- --------------------------------------------------------
 
 --
--- Table structure for table `status`
---
-
-CREATE TABLE `status` (
-  `status_id` int(11) NOT NULL,
-  `e_id` int(11) DEFAULT NULL,
-  `s_id` int(11) DEFAULT NULL,
-  `r_id` int(11) DEFAULT NULL,
-  `s_info` varchar(255) DEFAULT NULL,
-  `r_info` varchar(255) DEFAULT NULL,
-  `r_stars` int(11) DEFAULT NULL,
-  `s_stars` int(11) DEFAULT NULL,
-  `final_status` varchar(100) DEFAULT NULL,
-  `buy_nobuy` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `user`
 --
 
@@ -231,15 +211,16 @@ CREATE TABLE `user` (
   `email` varchar(100) NOT NULL,
   `address` text NOT NULL,
   `pword` varchar(50) NOT NULL,
-  `profile_img` varchar(100) NOT NULL
+  `profile_img` varchar(100) NOT NULL,
+  `creditpoints` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `fname`, `cname`, `role`, `contact`, `email`, `address`, `pword`, `profile_img`) VALUES
-(1, 'Rohan Shah', 'Rohan Corps', 'user', 8169885434, 'rohan27@somaiya.edu', 'Byculla, Mumbai', 'rohan', 'http://[::1]/CI_EWM/uploads/profilepic/user/KJSCE1.JPG');
+INSERT INTO `user` (`id`, `fname`, `cname`, `role`, `contact`, `email`, `address`, `pword`, `profile_img`, `creditpoints`) VALUES
+(1, 'Rohan Shah', 'Rohan Corps', 'user', 8169885434, 'rohan27@somaiya.edu', 'Byculla, Mumbai', 'rohan', 'http://[::1]/CI_EWM/uploads/profilepic/user/KJSCE1.JPG', 3);
 
 --
 -- Indexes for dumped tables
@@ -281,15 +262,6 @@ ALTER TABLE `recycler`
 ALTER TABLE `service`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `Email_id` (`email`);
-
---
--- Indexes for table `status`
---
-ALTER TABLE `status`
-  ADD PRIMARY KEY (`status_id`),
-  ADD KEY `e_id` (`e_id`),
-  ADD KEY `s_id` (`s_id`),
-  ADD KEY `r_id` (`r_id`);
 
 --
 -- Indexes for table `user`
@@ -339,28 +311,10 @@ ALTER TABLE `service`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `status`
---
-ALTER TABLE `status`
-  MODIFY `status_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `status`
---
-ALTER TABLE `status`
-  ADD CONSTRAINT `status_ibfk_1` FOREIGN KEY (`e_id`) REFERENCES `ewaste` (`e_id`),
-  ADD CONSTRAINT `status_ibfk_2` FOREIGN KEY (`s_id`) REFERENCES `service` (`id`),
-  ADD CONSTRAINT `status_ibfk_3` FOREIGN KEY (`r_id`) REFERENCES `recycler` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
