ff003455c6217f6b092730c21737c3a77c5e3818
rohan-op <rohan27@somaiya.edu>
2021-04-15 14:31:06 +0530

    UI

--- application/views/user/search_user.php
+++ application/views/user/search_user.php
@@ -7,82 +7,59 @@
         <li class="breadcrumb-item active">Buying</li>
     </ol>
     <br>
-    <h2 class="text-primary">Refurbished Product</h2>
-    <br>
-    <h3 class="text-primary">Search Results</h3>
-    <br>
-    <label>Sort By</label>     
+    <label for="exampleInputEmail1">Sort By / Search By </label> <br><br> 
     <?= form_open('user/search',['class'=>'form-inline my-2 my-lg-0']) ?> 
     <table>
       <tr>
-        <td>
+        <td>    
           <?php $options = array(1=>'Price:Lowest to Highest',2=>'Price:Highest to Lowest');?>
           <?php echo form_dropdown('sortby',$options,'Sort By',['class'=>'custom-select']); ?>
         </td>
-      </tr>
-      <tr></tr>
-      <tr>
         <td>
-          <?php echo form_input(['name'=>'search','type'=>'text','class'=>'form-control mr-sm-2','placeholder'=>'Search','value'=>set_value('search')]); ?>      
-          <?php echo form_submit(['name'=>'submit','class'=>'btn btn-outline-secondary','style'=>'color:black;','value'=>'Submit']); ?>      
+          <?php echo form_input(['name'=>'search','type'=>'text','class'=>'form-control mr-sm-2','placeholder'=>'Name Or Category','value'=>set_value('search')]); ?>
+        </td>
+        <td>
+          <?php echo form_submit(['name'=>'submit','class'=>'btn btn-primary','style'=>'color:white;','value'=>'Submit']); ?> 
         </td>
       </tr>
     </table>
     <?= form_close(); ?>
-    <br><br>
         <div class="row">
+        <section class="text-gray-600 body-font">
+          <div class="container px-5 py-24 mx-auto">
+            <h2 class="text-primary">Search Results</h2><br>
+            <div class="flex flex-wrap -m-4">
         <?php if( count($products) ):
         $count=$this->uri->segment(3);
         foreach ($products as $products): ?>
-
-        <div class="col-6" style="margin-bottom: 40px;">    
-            <div class="card border-primary mb-3" style="max-width: 25rem;border: none; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2); transition: 0.3s;">        
-              <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <img class="d-block w-100" src="<?= $products->p_img1 ?>" alt="First slide">
-                  </div>
-                  <div class="carousel-item">
-                    <img class="d-block w-100" src="<?= $products->p_img1 ?>" alt="Second slide">
-                  </div>
-                  <div class="carousel-item">
-                    <img class="d-block w-100" src="<?= $products->p_img1 ?>" alt="Third slide">
-                  </div>
-                </div>
-              </div>
-              <div class="card-body">
-                <h4 class="card-title"><?= $products->p_name; ?></h4>
-                <p class="card-text">Type:<strong><?= $products->p_type; ?></strong></p>
-                <p class="card-text">Date:<strong> 3/11/2020</strong></p>
-                <p class="card-text">Price:<strong><?= $products->p_cost; ?></strong></p>
-                <div>
-                  <table>
-                    <tr>
-                      <td>
-                        <?= anchor("user/productDetails/{$products->p_id}",'More Details',['class'=>'btn btn-info']);?>
-                      </td>
-                      <td>
-                        <?= anchor("user/addtoCart/{$products->p_id}",'Add to Cart',['class'=>'btn btn-success']);?>
-                      </td>
-                    </tr>
-                  </table>
-                </div> 
+            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <a class="block relative h-53 rounded overflow-hidden">
+              <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="<?= $products->p_img1 ?>">
+            </a>
+              <div class="mt-4">
+                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1"><?= $products->p_type; ?></h3>
+                <h2 class="text-gray-900 title-font text-lg font-medium"><?= $products->p_name; ?></h2>
+                <p class="mt-1"><?= $products->p_cost; ?></p>
+                <?= anchor("user/addtoCart/{$products->p_id}",'Add to Cart',['class'=>'btn btn-success']);?>
+                &nbsp;<br><br>
+                <?= anchor("user/productDetails/{$products->p_id}",'More Details',['class'=>'btn btn-info']);?>
               </div>
             </div>
+        <?php endforeach; ?>
+            
+              <?php else: ?>
+                  <tr>
+                    <td colspan="3">No records found.</td>
+                  </tr>
+              <?php endif; ?> 
+            </div>
           </div>
-
-            <?php endforeach; ?>
-            <?php else: ?>
-                <tr>
-                  <td colspan="3">No records found.</td>
-                </tr>
-            <?php endif; ?>   
+        </section>  
         </div>
 
       <div>
         <?= $this->pagination->create_links(); ?>
       </div>
   </div>
-</div>    
-</body>
+   
 <?php include('footer.php');?>
