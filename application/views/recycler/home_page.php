2ebcac104a7a4479525fc4ecdac6623165220496
pranavbhat12 <pranavbhat01@gmail.com>
2021-04-18 18:24:31 +0530

    Updated some links and UI

--- application/views/recycler/home_page.php
+++ application/views/recycler/home_page.php
@@ -41,7 +41,7 @@
 			<br>
 		<?php endforeach; ?></p>
         <div class="flex items-center flex-wrap pb-4 mb-4 border-b-2 border-gray-100 mt-auto w-full">
-          <a class="text-green-500 inline-flex items-center" href="historyProducts">See More
+          <a class="text-green-500 inline-flex items-center" href="recycledProducts">See More
             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path d="M5 12h14"></path>
               <path d="M12 5l7 7-7 7"></path>
@@ -61,7 +61,7 @@
 	<?php endforeach; ?></p>
 	<br>
         <div class="flex items-center flex-wrap pb-4 mb-4 border-b-2 border-gray-100 mt-auto w-full">
-          <a class="text-green-500 inline-flex items-center" href="yourDonations">See More
+          <a class="text-green-500 inline-flex items-center" href="requestPage">See More
             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path d="M5 12h14"></path>
               <path d="M12 5l7 7-7 7"></path>
--- application/views/service/header_service.php
+++ application/views/service/header_service.php
@@ -22,7 +22,7 @@
 </head>
 <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="#"><img height=110 width=110 src=<?= base_url('/assets/images/logo.png')?> ></a>
+  <a class="navbar-brand" href="homePage"><img height=110 width=110 src=<?= base_url('/assets/images/logo.png')?> ></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
